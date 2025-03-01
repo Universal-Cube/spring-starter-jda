8c0b356205b4edae8e072fe957c47d8b281dbf59
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2025-03-01 23:26:56 +0100

    `feat(commands): add BotCommand and BotCommandExecutor interfaces`
    ---
    Introduce foundational interfaces for defining and executing Discord bot commands, providing structure for command data creation and execution.

---
+++ src/main/java/org/universalcube/spring_starter_discord/commands/interfaces/BotCommand.java
@@ -0,0 +1,9 @@
+package org.universalcube.spring_starter_discord.commands.interfaces;
+
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import org.springframework.lang.NonNull;
+
+public interface BotCommand extends BotCommandExecutor {
+	@NonNull
+	CommandData createCommandData();
+}
---
+++ src/main/java/org/universalcube/spring_starter_discord/commands/interfaces/BotCommandExecutor.java
@@ -0,0 +1,9 @@
+package org.universalcube.spring_starter_discord.commands.interfaces;
+
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import org.springframework.lang.NonNull;
+
+public interface BotCommandExecutor {
+	void onExecute(@NonNull Member sender, @NonNull SlashCommandInteractionEvent event);
+}
