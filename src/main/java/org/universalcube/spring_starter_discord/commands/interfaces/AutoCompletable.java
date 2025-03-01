d3eaf44ee3199276e320063a0e6a3de54211882d
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2025-03-01 23:27:14 +0100

    `feat(commands): add AutoCompletable interface for handling command auto-complete events`
    ---
    
    Added a new `AutoCompletable` interface to facilitate handling of `CommandAutoCompleteInteractionEvent` in the Discord command system.

---
+++ src/main/java/org/universalcube/spring_starter_discord/commands/interfaces/AutoCompletable.java
@@ -0,0 +1,8 @@
+package org.universalcube.spring_starter_discord.commands.interfaces;
+
+import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
+import org.springframework.lang.NonNull;
+
+public interface AutoCompletable {
+	void onAutoComplete(@NonNull CommandAutoCompleteInteractionEvent event);
+}
