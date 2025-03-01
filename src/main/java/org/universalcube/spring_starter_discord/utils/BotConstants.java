c495b542500af2758879f1e13aecacfeea7c4d7e
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2025-03-01 19:47:28 +0100

    `feat(utils): add BotConstants for emoji definitions`
    ---
    Introduce `BotConstants` class to define reusable constant emoji and Unicode values for back and forward navigation.

---
+++ src/main/java/org/universalcube/spring_starter_discord/utils/BotConstants.java
@@ -0,0 +1,11 @@
+package org.universalcube.spring_starter_discord.utils;
+
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+
+public final class BotConstants {
+	public static final String BACK_ARROW_UNICODE = "\u2B05";
+	public static final String FORWARD_ARROW_UNICODE = "\u27A1";
+
+	public static final Emoji BACK_ARROW_EMOJI = Emoji.fromUnicode(BACK_ARROW_UNICODE);
+	public static final Emoji FORWARD_ARROW_EMOJI = Emoji.fromUnicode(FORWARD_ARROW_UNICODE);
+}
