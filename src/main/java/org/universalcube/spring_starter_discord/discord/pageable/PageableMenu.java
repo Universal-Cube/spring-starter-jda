cc74b537adba33b534c60dae65ff8b554aac24da
Purpose <111461393+Purpose-Dev@users.noreply.github.com>
2025-03-02 01:37:17 +0100

    feat(discord): add abstract PageableMenu for paginated content navigation
    ---
    Introduced an abstract `PageableMenu` class to handle paginated content rendering with support for navigating to previous and next pages while enforcing page bounds.

---
+++ src/main/java/org/universalcube/spring_starter_discord/discord/pageable/PageableMenu.java
@@ -0,0 +1,36 @@
+package org.universalcube.spring_starter_discord.discord.pageable;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public abstract class PageableMenu {
+	protected int maxPage = Integer.MAX_VALUE;
+	protected final AtomicInteger currentPage;
+
+	protected PageableMenu() {
+		this(1);
+	}
+
+	protected PageableMenu(int startPage) {
+		this.currentPage = new AtomicInteger(startPage);
+	}
+
+	public abstract void render(int page);
+
+	public int previousPage() {
+		int initialPage = this.currentPage.intValue();
+		if (initialPage <= 1)
+			return initialPage;
+
+		this.render(this.currentPage.decrementAndGet());
+		return initialPage - 1;
+	}
+
+	public int nextPage() {
+		int initialPage = this.currentPage.intValue();
+		if (initialPage >= this.maxPage)
+			return initialPage;
+
+		this.render(this.currentPage.incrementAndGet());
+		return initialPage + 1;
+	}
+}
