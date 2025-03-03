dac31526b25b9ba2fe0f19e0d1863ad11f247894
Purpose <riyane.dev@gmail.com>
2025-03-03 21:00:04 +0100

    feat(permissions): add owner and root user validation methods
    ---
    Added methods to validate root and owner users (`isRoot`, `isOwner`). Added getter methods for root ID (`getRoot`) and owners (`getOwners`). Enhanced null-checks for `JdaConfigurationProperties`.

--- src/main/java/org/universalcube/spring_starter_discord/discord/permissions/JdaPermissionUtils.java
+++ src/main/java/org/universalcube/spring_starter_discord/discord/permissions/JdaPermissionUtils.java
@@ -3,13 +3,15 @@ package org.universalcube.spring_starter_discord.discord.permissions;
 import org.universalcube.spring_starter_discord.properties.JdaConfigurationProperties;
 import org.universalcube.spring_starter_discord.properties.Owners;
 
+import java.util.Collections;
 import java.util.Objects;
+import java.util.Set;
 
 public class JdaPermissionUtils {
 	private final Owners ownersSettings;
 
 	public JdaPermissionUtils(JdaConfigurationProperties properties) {
-		if (Objects.isNull(properties.getOwners())) {
+		if (Objects.isNull(properties) || Objects.isNull(properties.getOwners())) {
 			throw new IllegalArgumentException("Properties cannot be null");
 		}
 		this.ownersSettings = properties.getOwners();
@@ -17,5 +19,21 @@ public class JdaPermissionUtils {
 
 	public boolean isEnabled() {
 		return this.ownersSettings.isEnabled();
+	}
+
+	public boolean isRoot(String userId) {
+		return ownersSettings.getRootId() != null && ownersSettings.getRootId().equals(userId);
+	}
+
+	public boolean isOwner(String userId) {
+		return ownersSettings.getIds().contains(userId) || isRoot(userId);
+	}
+
+	public String getRoot() {
+		return ownersSettings.getRootId();
+	}
+
+	public Set<String> getOwners() {
+		return Collections.unmodifiableSet(ownersSettings.getIds());
 	}
 }
