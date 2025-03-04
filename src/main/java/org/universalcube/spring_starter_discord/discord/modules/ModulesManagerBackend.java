ba7f01342eac804e15559e58dd6b52098c860b12
Purpose <riyane.dev@gmail.com>
2025-03-04 04:40:53 +0100

    feat(modules): enhance module management with new utility methods and improved initialization
    ---
    Added methods for retrieving, enabling, and reloading modules. Improved module initialization by creating instances dynamically and introducing error handling with logging.

--- src/main/java/org/universalcube/spring_starter_discord/discord/modules/ModulesManagerBackend.java
+++ src/main/java/org/universalcube/spring_starter_discord/discord/modules/ModulesManagerBackend.java
@@ -1,13 +1,16 @@
 package org.universalcube.spring_starter_discord.discord.modules;
 
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import org.springframework.context.ApplicationContext;
 import org.universalcube.spring_starter_discord.discord.modules.annotations.BotModuleComponent;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Slf4j
 public class ModulesManagerBackend {
 	private final JDA jda;
 	private final ApplicationContext applicationContext;
@@ -18,15 +21,75 @@ public class ModulesManagerBackend {
 		this.applicationContext = applicationContext;
 	}
 
+	public Map<String, BotModule> getModules() {
+		return Collections.unmodifiableMap(modules);
+	}
+
+	public Map<String, BotModule> getEnabledModules() {
+		return modules.entrySet()
+				.stream()
+				.filter(entry -> entry.getValue().isEnabled())
+				.collect(ConcurrentHashMap::new, (map, entry) -> map.put(entry.getKey(), entry.getValue()), Map::putAll);
+	}
+
+	public BotModule getModule(String name) {
+		return modules.get(name);
+	}
+
+	public synchronized void addModule(BotModule module) {
+		modules.putIfAbsent(module.getName(), module);
+		jda.addEventListener(module);
+
+		if (module.isEnabled()) {
+			module.onEnable();
+		}
+	}
+
+	public void reloadModules() {
+		log.info("Reloading Discord modules...");
+		getEnabledModules().forEach(((s, module) -> module.onDisable()));
+		modules.clear();
+		init();
+		log.info("Discord modules reloaded");
+	}
+
+	public synchronized void reloadModule(String name) {
+		BotModule module = modules.get(name);
+
+		if (module != null) {
+			module.onDisable();
+			modules.remove(name);
+			init();
+			log.info("Discord module {} reloaded", name);
+		}
+	}
+
 	@PostConstruct
 	public void init() {
 		Map<String, Object> beans = applicationContext.getBeansWithAnnotation(BotModuleComponent.class);
+
 		beans.forEach((name, bean) -> {
 			if (bean instanceof BotModule module) {
 				BotModuleComponent annotation = module.getClass().getAnnotation(BotModuleComponent.class);
-				String moduleName = annotation.name();
-				modules.putIfAbsent(moduleName, module);
-				jda.addEventListener(module);
+				if (annotation == null) {
+					return;
+				}
+
+				try {
+					BotModule newModule = module.getClass()
+							.getConstructor(String.class, String.class, boolean.class, boolean.class, boolean.class)
+							.newInstance(annotation.name(), annotation.description(), annotation.enabled(), annotation.devOnly(), annotation.botOwnerOnly());
+
+					modules.putIfAbsent(annotation.name(), newModule);
+					jda.addEventListener(newModule);
+
+					if (newModule.isEnabled()) {
+						newModule.onEnable();
+					}
+				} catch (Exception e) {
+					log.error("Error initializing module {}", annotation.name(), e);
+					throw new RuntimeException("Error initializing module " + annotation.name(), e);
+				}
 			}
 		});
 	}
