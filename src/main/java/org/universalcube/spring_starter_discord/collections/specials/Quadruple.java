fea3891bfbac5b038e4e0972ded9bc5b1b2d0cdb
Purpose <riyane.dev@gmail.com>
2025-03-05 08:27:30 +0100

    `docs(collections): add class-level Javadoc comments for Triple and Quadruple`
    ---
    Added detailed Javadoc comments to Triple and Quadruple classes to describe their purpose, immutability, and generic type parameters.

--- src/main/java/org/universalcube/spring_starter_discord/collections/specials/Quadruple.java
+++ src/main/java/org/universalcube/spring_starter_discord/collections/specials/Quadruple.java
@@ -2,5 +2,15 @@ package org.universalcube.spring_starter_discord.collections.specials;
 
 import java.io.Serializable;
 
+/**
+ * A generic immutable data structure representing a tuple of four elements.
+ * This class can store four values of potentially different types.
+ * The order of the elements is maintained as provided during object creation.
+ *
+ * @param <T> the type of the first element
+ * @param <U> the type of the second element
+ * @param <V> the type of the third element
+ * @param <W> the type of the fourth element
+ */
 public record Quadruple<T, U, V, W>(T first, U second, V third, W fourth) implements Serializable {
 }
--- src/main/java/org/universalcube/spring_starter_discord/collections/specials/Triple.java
+++ src/main/java/org/universalcube/spring_starter_discord/collections/specials/Triple.java
@@ -2,5 +2,14 @@ package org.universalcube.spring_starter_discord.collections.specials;
 
 import java.io.Serializable;
 
+/**
+ * A generic immutable data structure representing a tuple of three elements.
+ * This class can store three values of potentially different types.
+ * The order of the elements is maintained as provided during object creation.
+ *
+ * @param <T> the type of the first element
+ * @param <U> the type of the second element
+ * @param <V> the type of the third element
+ */
 public record Triple<T, U, V>(T first, U second, V third) implements Serializable {
 }
